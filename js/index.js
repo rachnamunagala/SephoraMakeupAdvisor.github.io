f7a77295555d1e66471a45f27cb3453a79a63c57
rachnamunagala <rachnamunagala@yahoo.com>
2016-06-18 20:30:31 -0400

    Spear Makeup Advisor
    This quiz will help choose the perfect makeup product for you!

---
+++ js/index.js
@@ -0,0 +1,34 @@
+var q5 = document.querySelector('[name=q5]')
+
+
+
+
+
+function showResults()
+{
+  if (q4a.checked==true && q5a.checked==true)
+  {
+    window.location = "http://www.sephora.com/pure-matte-lipstick-P273416"  
+  }
+   if (q4b.checked==true && q5a.checked==true)
+  {
+    window.location ="http://www.sephora.com/camera-ready-bb-cream-spf-35-P300339?skuId=1574896&icid2=D=c6:products%20grid:p300339"  
+  }
+  if (q4b.checked==true && q5b.checked==true)
+  {
+    window.location ="http://www.sephora.com/tarteist-trade-glossy-lip-paint-P406778?skuId=1787159&icid2=D=c6:products%20grid:p406778" 
+  }
+     if (q4a.checked==true && q5b.checked==true)
+  {
+    window.location = "http://www.sephora.com/naked-smoky-P398419?skuId=1724350&icid2=D=c6:products%20grid:p398419"
+  }
+ if (q4a.checked==true && q5c.checked==true)
+  {
+   window.location ="http://www.sephora.com/glow-kit-P406576"  
+  }
+if (q4b.checked==true && q5c.checked==true)
+ {
+ window.location = "http://www.sephora.com/semi-sweet-chocolate-bar-P392009?skuId=1664721&icid2=D=c6:products%20grid:p392009"
+ }
+
+}
